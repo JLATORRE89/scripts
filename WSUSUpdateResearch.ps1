864519c967991f8ee36254a2589828a74287d8e9
Jason <jlatorre1989@hotmail.com>
2020-03-17 22:39:50 -0400

    Add files via upload
    Commands and notes about WSUS in Powershell

---
+++ WSUSUpdateResearch.ps1
@@ -0,0 +1,20 @@
+﻿# Get Windows Update Version
+Get-WUAVersion
+# Check for Pending reboot
+Get-WUIsPendingReboot
+# Last install date
+Get-WULastInstallationDate
+# Last successful Scan Date
+Get-WULastScanSuccessDate
+# Start a scan of updates against the current system
+Start-WUScan 
+# Install a specific KB.
+Install-WUUpdates -AsJob KB4537572
+
+# Get supportedmodule commandlets
+Get-Command -Module WindowsUpdateProvider
+
+# https://docs.microsoft.com/en-us/windows/win32/wua_sdk/portal-client
+# Get update service object information
+$ServiceManager = (New-Object -com "Microsoft.Update.ServiceManager")
+$ServiceManager.Services
