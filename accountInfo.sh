b5e58aa4076858102af369751a1c436e781c991f
Jason <jason@jasonlatorre.com>
2022-12-18 21:47:23 -0600

    Update accountInfo.sh

--- accountInfo.sh
+++ accountInfo.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 #Depends:0 bash-4.2.46-34, sed-4.2.2-7, gawk-4.0.2-4
-sed 's/:/ /' /etc/passwd | awk '{print }'
-chage -l root | grep expires
+
+# Get user list from /etc/passwd
+USERS=$(sed 's/:/ /' /etc/passwd | awk '{print $1 }')
+# generate data for each user
+for USER in $USERS
+do
+        echo Username: $USER
+        chage -l $USER | grep expires
+done
